3507c1ac51d9af751612d2f1f77d7d1d2909b5c3
ziap <67962871+ziap@users.noreply.github.com>
2021-12-28 07:10:15 +0700

    Change user data directory for self-extract, improve scripts

--- Wrapper.cs
+++ Wrapper.cs
@@ -12,20 +12,23 @@ class Wraper : System.Windows.Forms.Form
         StartPosition = FormStartPosition.CenterScreen;
         InitWebView();
     }
-    
-    async void InitWebView() {
-        var webView = new WebView2() {Dock = DockStyle.Fill};
-        await webView.EnsureCoreWebView2Async();
-        #if(DEBUG)
-            webView.Source = new Uri("http://localhost:3000");
-        #else
-            webView.CoreWebView2.SetVirtualHostNameToFolderMapping("0.0.0.0", Application.StartupPath + "wwwroot", CoreWebView2HostResourceAccessKind.Allow);
-            webView.Source = new Uri("https://0.0.0.0/index.html");
-        #endif
+
+    async void InitWebView()
+    {
+        var webView = new WebView2() { Dock = DockStyle.Fill };
+        var env = await CoreWebView2Environment.CreateAsync(null, Path.Combine(Application.StartupPath, "userdata"), null);
+        await webView.EnsureCoreWebView2Async(env);
+#if (DEBUG)
+        webView.Source = new Uri("http://localhost:3000");
+#else
+        webView.CoreWebView2.SetVirtualHostNameToFolderMapping("0.0.0.0", Path.Combine(Application.StartupPath, "wwwroot"), CoreWebView2HostResourceAccessKind.Allow);
+        webView.Source = new Uri("https://0.0.0.0/index.html");
+        webView.CoreWebView2.Settings.AreDevToolsEnabled = false;
+        webView.CoreWebView2.Settings.IsStatusBarEnabled = false;
+#endif
         webView.CoreWebView2.DocumentTitleChanged += (sender, e) => Text = webView.CoreWebView2.DocumentTitle;
         Controls.Add(webView);
     }
-
 
     /// <summary>
     ///  The main entry point for the application.
