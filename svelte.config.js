ee208879fa7df98d084c1ad089f19a2b7b47b117
ziap <67962871+ziap@users.noreply.github.com>
2021-12-31 15:40:31 +0700

    Add PostCSS - Autoprefixer

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			pages: 'wwwroot',
-		}),
+  kit: {
+    adapter: adapter({
+      pages: "wwwroot",
+    }),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+  },
+
+  preprocess: [preprocess({})],
 };
 
 export default config;
